f3236a811314b790ca0a85bed161575c0c6f8052
Chris Andrew <chrisandrew_dev@protonmail.com>
2021-02-17 20:35:00 +0100

    Apply styles

--- components/Header.js
+++ components/Header.js
@@ -3,8 +3,8 @@ import styles from '../styles/Header.module.scss'
 const Header = ({heading, desc}) => {
   return ( 
     <header className={styles.header}>
-      <h1>{heading}</h1>
-      <p>{desc}</p>
+      <h1 className={styles.title}>{heading}</h1>
+      <p className={styles.description}>{desc}</p>
     </header>
   );
 }
