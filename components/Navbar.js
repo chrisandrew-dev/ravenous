0fe94eced481f6824336c0e3a89cb141f3a5e410
Chris Andrew <chrisandrew_dev@protonmail.com>
2021-02-17 20:34:33 +0100

    Refactor nav links, styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,25 +2,31 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Navbar.module.scss'
 
-const Navbar = () => {
-  return ( 
-    <div className={styles.navbar}>
-      <nav>
-        <div className={styles.logo}>
-          <Image 
-            src='/logo.svg' 
-            height={66} 
-            width={80} 
-          />
-          <h1>Ravenous.</h1>
-        </div>
-        <ul>
-          <li><Link href='/'><a className={styles.active}>Search</a></Link></li>
-          <li><Link href='/about'><a>About</a></Link></li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
- 
+const Navbar = () => (
+  <div className={styles.navbar}>
+    <nav>
+      <div className={styles.logo}>
+        <Image
+          src='/logo.svg'
+          height={66}
+          width={80}
+        />
+        <h1>Ravenous.</h1>
+      </div>
+      <ul>
+        <li>
+          <Link href='/'>
+            <a>Search</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+)
+
 export default Navbar;
