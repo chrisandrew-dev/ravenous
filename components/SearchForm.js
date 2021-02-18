82eabb27e5c7bbb5559645a5f2493c6cdbb1c55d
Chris Andrew <chrisandrew_dev@protonmail.com>
2021-02-18 17:15:01 +0100

    Add SearchForm comp

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -1,9 +1,60 @@
 const SearchForm = () => {
-  return ( 
-    <form>
-      I am the Search Form
+  return (
+    <form className={styles.form}>
+      <fieldset>
+        <input
+          type='radio'
+          id='best_match'
+          value='best_match'
+          name='sort-method'
+          defaultChecked
+        />
+        <label htmlFor='best_match'>Best match</label>
+
+        <input
+          type='radio'
+          id='rating'
+          value='rating'
+          name='sort-method'
+        />
+        <label htmlFor='rating'>Highest rated</label>
+
+        <input
+          type='radio'
+          id='review_count'
+          value='review_count'
+          name='sort-method'
+        />
+        <label htmlFor='review_count'>Most reviewed</label>
+      </fieldset>
+
+      <fieldset>
+        <label htmlFor='term'>Search term:</label>
+        <input
+          required
+          type='text'
+          min='1'
+          max='50'
+          placeholder="What are you craving?"
+          name='term'
+          id='term'
+        ></input>
+
+        <label htmlFor='term'>Search location:</label>
+        <input
+          required
+          type='text'
+          min='1'
+          max='50'
+          placeholder="Where should we look?"
+          name='location'
+          id='location'
+        ></input>
+      </fieldset>
+
+      <input type='submit' value='Search' />
     </form>
-  );
+  )
 }
- 
+
 export default SearchForm;
