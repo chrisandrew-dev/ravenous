f1d0acd6592364fa7d2217e085284c18739e986e
Chris Andrew <chrisandrew_dev@protonmail.com>
2021-02-25 19:20:05 +0100

    Create useCurrentLocation, useYelp helpers

---
+++ helpers.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+
+const useCurrentLocation = (options = {}) => {
+  const [geolocation, setGeolocation] = useState()
+  const [error, setError] = useState()
+
+  const handleSuccess = position => {
+    const { latitude, longitude } = position.coords
+    setGeolocation({ latitude, longitude })
+  }
+
+  const handleError = error => {
+    setError(error.message)
+  }
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setError('Geolocation unsupported.')
+      return
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      handleSuccess,
+      handleError,
+      options
+    )
+  }, [options])
+
+  return { geolocation, error }
+}
+
+const useYelpBusinessSearch = async (location, searchTerm, searchArea, sortMethod) => {
+	const API_KEY = @yelp-fusion-api-key
+	console.log(API_KEY)
+	const baseUrl = 'https://api.yelp.com/v3/businesses/search'
+	const baseQuery = `?searchTerm=${searchTerm}&sort_by=${sortMethod}&limit=50`
+	const locationQuery = searchArea
+	  ? `&location=${searchArea}`
+	  : `&latitude=${location.latitude}&longitude=${location.longitude}&radius=10000`
+	const headers = { Authorization: 'Bearer ${API_KEY}'}
+
+	const res = await fetch(baseUrl + baseQuery + locationQuery, { headers: headers })
+	const businesses = await res.json()
+	return businesses
+}
+
+export { useCurrentLocation, useYelpBusinessSearch }
