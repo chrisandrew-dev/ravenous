a3c2942e3e341e98cc26646d974f55fe96ef1c2c
Chris Andrew <chrisandrew_dev@protonmail.com>
2021-02-25 19:21:29 +0100

    Update styles

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -2,9 +2,11 @@ import Image from 'next/image'
 
 const HeroBanner = ({ children, src }) => {
 	const styles = {
-		background: `url(${src}) center / cover`,
+		background: `url(${src}) bottom left / cover`,
 		width: '100%',
-		padding: '3em 1em'
+		padding: '2em 1em',
+		borderRadius: '0.25em',
+		marginBlock: '1rem',
 	}
 
 	return (
